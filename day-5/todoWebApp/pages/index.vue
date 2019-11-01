838e20f5f2d7d58f58dce3e091cf1d182fa3738b
miqdude <miqdadabdurhman@gmail.com>
2019-11-01 15:09:29 +0700

    added simple form

---
+++ day-5/todoWebApp/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="">
+    <header>
+      TodoWebApp
+    </header>
+    <section class="addList">
+      <input type="text">
+      <button @click="newTodo">+</button>
+    </section>
+    <section class="todoList">
+      <ul >
+        <li>
+          <label for="">Do nothing</label>
+          <button>-</button>
+        </li>
+        <li>
+          <label for="">Do somthing</label>
+          <button>-</button>
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      books:{}
+    }
+  },
+  methods:{
+    newTodo(){
+      console.log("new todo")
+    }
+  }
+}
+</script>
